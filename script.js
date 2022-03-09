771eaab74944fbb9d46bcfa2d8a173ef08f796ad
aalsayad <74960405+aalsayad@users.noreply.github.com>
2022-03-10 02:02:16 +0300

    observer decreased to 5px and fixed logo issue on firefox

--- script.js
+++ script.js
@@ -1,135 +1,129 @@
 //Is my script.js working?
-console.log('Script.js is now linked & running!')
+console.log("Script.js is now linked & running!");
 
-const nvbar = document.querySelector('.nvbar');
-const nvbarLinksWrapper = document.querySelector('.nv-links-wrapper');
-const body = document.querySelector('body');
-
+const nvbar = document.querySelector(".nvbar");
+const nvbarLinksWrapper = document.querySelector(".nv-links-wrapper");
+const body = document.querySelector("body");
 
 //top of the page navbar change - transparent to white
 
-nvbarLinksWrapper.addEventListener('mouseenter',()=>{
-    if(AmIatTop == "True" && !mobileView){
-    nvbar.classList.remove('transparent');
-    }
+nvbarLinksWrapper.addEventListener("mouseenter", () => {
+  if (AmIatTop == "True" && !mobileView) {
+    nvbar.classList.remove("transparent");
+  }
 });
-nvbarLinksWrapper.addEventListener('mouseleave',()=>{
-    if(AmIatTop == "True" && !mobileView){
-        nvbar.classList.add('transparent');
-    } 
+nvbarLinksWrapper.addEventListener("mouseleave", () => {
+  if (AmIatTop == "True" && !mobileView) {
+    nvbar.classList.add("transparent");
+  }
 });
 
 //Observer
 var AmIatTop = "True"; //Boolean Variable that changes based  on observer in other words: Are we on tpo of the page or not?
-const ToggleWhiteNav = document.querySelector('.observer-div');//this class is the class of the div that will be at the top of the page
+const ToggleWhiteNav = document.querySelector(".observer-div"); //this class is the class of the div that will be at the top of the page
 const HeroSectionOptions = {};
-const HeroSectionObserver = new IntersectionObserver(function(
-    entries,
-    HeroSectionObserver
-){
-    entries.forEach(entry => {
-       // console.log(entry.target); //checking if console fires up on intersection of redbox
-       if(entry.isIntersecting){
-        AmIatTop = "True";
-        console.log("AmIatTop = " + AmIatTop)
-       } else{
-        AmIatTop = "False";
-        console.log("AmIatTop = " + AmIatTop)
-       }
-    })
+const HeroSectionObserver = new IntersectionObserver(function (
+  entries,
+  HeroSectionObserver
+) {
+  entries.forEach((entry) => {
+    // console.log(entry.target); //checking if console fires up on intersection of redbox
+    if (entry.isIntersecting) {
+      AmIatTop = "True";
+      console.log("AmIatTop = " + AmIatTop);
+    } else {
+      AmIatTop = "False";
+      console.log("AmIatTop = " + AmIatTop);
+    }
+  });
 },
 HeroSectionOptions);
 
 HeroSectionObserver.observe(ToggleWhiteNav);
 
 //Burger-Menu
-const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
 
-menuBtn.addEventListener('click', ()=>{
-  if(!menuOpen){
-    nvbar.classList.remove('transparent');
-    productsNvLink.classList.add('default-open'); //Activate Products DD Link Wrapper Hover Effect
-    productsDdDiv.classList.add('default-open'); //Activate Products DD Div Hovered Effect
-    menuBtn.classList.add('open');
-    nvbarLinksWrapper.classList.add('open');
-    body.style.overflow = 'hidden';//removing the scrollbar if the menu is open
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    nvbar.classList.remove("transparent");
+    productsNvLink.classList.add("default-open"); //Activate Products DD Link Wrapper Hover Effect
+    productsDdDiv.classList.add("default-open"); //Activate Products DD Div Hovered Effect
+    menuBtn.classList.add("open");
+    nvbarLinksWrapper.classList.add("open");
+    body.style.overflow = "hidden"; //removing the scrollbar if the menu is open
     menuOpen = true;
-  }else{
-    menuBtn.classList.remove('open');
-    nvbarLinksWrapper.classList.remove('open');
-    body.style.overflow = 'unset';//adding back the scrollbar if the menu is closed
+  } else {
+    menuBtn.classList.remove("open");
+    nvbarLinksWrapper.classList.remove("open");
+    body.style.overflow = "unset"; //adding back the scrollbar if the menu is closed
     menuOpen = false;
-    if(AmIatTop =="True"){
-        nvbar.classList.add('transparent');
+    if (AmIatTop == "True") {
+      nvbar.classList.add("transparent");
     }
   }
 });
 
-
 //Hide or Show Navbar on scroll - That also checks if Navbar is Open in Mobile View
-const productsDdDiv = document.getElementById('products-dd-div')
-const productsNvLink = document.getElementById('products')
+const productsDdDiv = document.getElementById("products-dd-div");
+const productsNvLink = document.getElementById("products");
 
-function NvScrollingAction(){
-    let lastScrollY = window.scrollY;
-    window.addEventListener('scroll', ()=>{
-        if(lastScrollY < window.scrollY && AmIatTop =="False"){
-            //console.log('Scrolling down')
-            nvbar.classList.remove('transparent');
-            nvbar.classList.add('move-up');
-        } else{
-            //console.log('Scrolling up')
-            nvbar.classList.remove('move-up');
-        }
-        lastScrollY = window.scrollY;
-    })
-};
+function NvScrollingAction() {
+  let lastScrollY = window.scrollY;
+  window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY && AmIatTop == "False") {
+      //console.log('Scrolling down')
+      nvbar.classList.remove("transparent");
+      nvbar.classList.add("move-up");
+    } else {
+      //console.log('Scrolling up')
+      nvbar.classList.remove("move-up");
+    }
+    lastScrollY = window.scrollY;
+  });
+}
 NvScrollingAction();
 
-
-//A function that automatically changes navbar to transparent when reached to the top 
+//A function that automatically changes navbar to transparent when reached to the top
 //This function works only on mobile because i have another function that makes the AmIatTop div height to 0
-function changeNvToTransparent(){
-    if(AmIatTop == "True"){
-        nvbar.classList.add('transparent');
-    }
+function changeNvToTransparent() {
+  if (AmIatTop == "True") {
+    nvbar.classList.add("transparent");
+  }
 }
-window.addEventListener('scroll', changeNvToTransparent);
-window.addEventListener('resize', changeNvToTransparent);
-window.addEventListener('load', changeNvToTransparent);
-
+window.addEventListener("scroll", changeNvToTransparent);
+window.addEventListener("resize", changeNvToTransparent);
+window.addEventListener("load", changeNvToTransparent);
 
 //Are we on mobile & Turning off transparent & turning off menuOpen
 var mobileView = false;
-function checkMobileView(){
-    if(document.body.offsetWidth < 1000){
-        mobileView = true;
-        console.log('We are now in mobile view');
-        nvbar.classList.remove('transparent');
-        //ToggleWhiteNav.style.height = "0px"; //Changing Observer Div to stop working when we are in mobile view
-        //ToggleWhiteNav.style.transform = "translateY(-100px)";
-        
-
-    }else{
-        mobileView = false;
-        console.log('We are now in desktop view');
-        //ToggleWhiteNav.style.height = "10px"; //Turning back on the observer div
-        //ToggleWhiteNav.style.transform = "translateY(0)";
-        menuBtn.classList.remove('open');
-        nvbarLinksWrapper.classList.remove('open');
-        menuOpen = false;
-        body.style.overflow='unset';
-    };
+function checkMobileView() {
+  if (document.body.offsetWidth < 1000) {
+    mobileView = true;
+    console.log("We are now in mobile view");
+    nvbar.classList.remove("transparent");
+    //ToggleWhiteNav.style.height = "0px"; //Changing Observer Div to stop working when we are in mobile view
+    //ToggleWhiteNav.style.transform = "translateY(-100px)";
+  } else {
+    mobileView = false;
+    console.log("We are now in desktop view");
+    //ToggleWhiteNav.style.height = "10px"; //Turning back on the observer div
+    //ToggleWhiteNav.style.transform = "translateY(0)";
+    menuBtn.classList.remove("open");
+    nvbarLinksWrapper.classList.remove("open");
+    menuOpen = false;
+    body.style.overflow = "unset";
+  }
 }
 checkMobileView();
-window.addEventListener('resize', checkMobileView);
-window.addEventListener('onload', checkMobileView);
+window.addEventListener("resize", checkMobileView);
+window.addEventListener("onload", checkMobileView);
 
 //On Hover other Mobile Tabs remove the default products tab hover effect
-nvbarLinksWrapper.addEventListener('mouseenter', ()=>{
-    if(document.body.offsetWidth < 1000){
-        productsNvLink.classList.remove('default-open');
-        productsDdDiv.classList.remove('default-open');
-    }
+nvbarLinksWrapper.addEventListener("mouseenter", () => {
+  if (document.body.offsetWidth < 1000) {
+    productsNvLink.classList.remove("default-open");
+    productsDdDiv.classList.remove("default-open");
+  }
 });
